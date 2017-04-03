59201252e259835c2f820f8993ee5b756ae84688
Pglinsman <pglinsman@gmail.com>
2017-04-03 03:17:23 -0400

    Added line graph

--- SmartSleeperApp/views.py
+++ SmartSleeperApp/views.py
@@ -1,8 +1,42 @@
+from __future__ import print_function # Python 2/3 compatibility
+import boto3
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
 from django.shortcuts import render
 
 # Create your views here.
+def parse_time(time):
+  time = time.replace("T", " ")
+  time = time.replace("Z", "")
+  time = time[0:len(time)-4]
+  # time = time[11:]
+  # time = "2012-02-24"
+  return time
+
 def home(request):
   context = {}
+  dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
+
+  table = dynamodb.Table('SensorData')
+
+  response = table.query(
+      KeyConditionExpression=Key('SensorId').eq("Temperature")
+  )
+
+  timeStamps = []
+  values = []
+  pair = []
+
+  for i in response['Items']:
+    timeStamps.append(parse_time(i['Timestamp']))
+    values.append(i['Value'])
+    #print(parse_time(i['Timestamp']), ":", i['Value'])
+
+  print(timeStamps)
+  pair = zip(timeStamps, values)
+  context['pair'] = pair
+
   return render(request, 'SmartSleeperApp/home.html', context)
 
 def alarm(request):
